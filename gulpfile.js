d49b6b1e911ef83fe0550138314b1817764bfb20
Danil <milostiviy.danil@yandex.ru>
2020-02-05 17:57:31 +0500

    разбираюсь с плагинами по оптимизации размеа изображений

--- gulpfile.js
+++ gulpfile.js
@@ -35,3 +35,66 @@ function build(cd) {
 
 exports.build = build;
 // exports.bonus = bonus;
+
+const imageResize = require('gulp-image-resize');
+const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const gulpif = require('gulp-if');
+
+//массив массивов [шириа, высота, имя картинки]
+const mobileWaybg = [[440, 575, 320], [650, 850, 480], [805, 1053, 600], [2600, 3400, 1500]];
+
+function mobile () {
+    mobileWaybg.forEach(function (item) {
+        gulp.src('test/test.jpg')
+            .pipe(imageResize({
+                imageMagick: true,
+                width: item[0],
+                height: item[1],
+                upscale : false,
+            }))
+            .pipe(gulpif(
+                !(item[2] === 1500),
+                imagemin([
+                    imagemin.mozjpeg({quality: 90, progressive: true})
+                ])
+            ))
+            .pipe(gulpif(
+                (item[2] === 1500),
+                imagemin([
+                    imagemin.mozjpeg({quality: 60, progressive: true})
+                ])
+            ))
+            // .pipe(imagemin([
+            //     imagemin.mozjpeg({quality: 60, progressive: true}),
+            //     imagemin.optipng({optimizationLevel: 5}),
+            // ]))
+            .pipe(rename(item[2] + '.jpg'))
+            .pipe(gulp.dest('dist'));
+    });
+};
+
+function test(cd) {
+    mobile();
+    cd();
+}
+
+exports.test = test;
+
+gulp.task('png', function() {
+    return gulp.src('output/episode295/*.png')
+        .pipe(imagemin([
+            imagemin.optipng({optimizationLevel: 7}),
+        ]))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('jpg', function() {
+    return gulp.src('output/episode295/*.jpg')
+        .pipe(imagemin([
+            imagemin.mozjpeg({quality: 85, progressive: true}),
+        ]))
+        .pipe(gulp.dest('./dist/'));
+});
+
+
